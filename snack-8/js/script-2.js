ef719a3156fcc05723bdefb9fcde00158b32e253
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 12:42:29 +0200

    esercizio da rifare

---
+++ snack-8/js/script-2.js
@@ -0,0 +1,17 @@
+/*sbagliato metodo applicato, da rifare snack*/
+
+
+const numOne = parseInt(prompt('inserisci un numero'));
+const numTwo = parseInt(prompt('inserisci un numero'));
+const numThree = parseInt(prompt('inserisci un numero'));
+const numFour = parseInt(prompt('inserisci un numero'));
+const numFive = parseInt(prompt('inserisci un numero'));
+const numSix = parseInt(prompt('inserisci un numero'));
+const isOdd = number => number % 2 !== 0;
+
+let numList = [numOne, numTwo, numThree, numFour, numFive, numSix];
+numList = [];
+
+if (numOne, numTwo, numThree, numFour, numFive, numSix === isOdd) {
+    console.log(numList.push(numOne, numTwo, numThree, numFour, numFive, numSix));
+}
