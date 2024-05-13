1490a6b61db329f17611aa7ec67026a3546b6a59
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-13 11:50:41 +0200

    creata logica per determinare il numero uno maggiore di due

---
+++ snack-1/js/script.js
@@ -0,0 +1,7 @@
+const numberOne = parseInt(prompt('inserisci numero uno'));
+const numberTwo = parseInt(prompt('inserisci numero due'));
+
+
+if (numberOne > numberTwo) {
+    console.log(numberOne + ' maggiore di ' + numberTwo);
+} 
